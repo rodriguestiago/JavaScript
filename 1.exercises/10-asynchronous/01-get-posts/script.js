ad8b4754f51fc4b72945f460e8a8200225344288
rodriguestiago <tiagogameiro15@gmail.com>
2020-01-20 16:58:06 +0100

    exo 10

--- 1.exercises/10-asynchronous/01-get-posts/script.js
+++ 1.exercises/10-asynchronous/01-get-posts/script.js
@@ -10,5 +10,61 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    
+    window.lib.getPosts()
+    
+    
+    
+    document.getElementById("run").addEventListener("click", timeFunction);
+    
+    function timeFunction() {
+ setTimeout(function(){ window.lib.getPosts(callback); }, 3000);
+}
+    
+
+    
+
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    document.getElementById("run").addEventListener("click", mySandwich);
+//    
+//    function callback (callback){
+//        
+//        
+//    }
+//    
+//    callback(window.lib.getPosts(callback));
+//    
+//    // your code here
+//    
+//    window.lib.getPosts(callback);
+//    
+//    setTimeout(window.lib.getPosts, 3000, callback)
+//    
+//    
+//    var rotator = function(){
+//  widget.Rotator.rotate();
+//  setTimeout(rotator,5000);
+//};
+//rotator();
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    
+//    function mySandwich(param1, param2, callback) {
+//  console.log(param1 + ', ' + param2);
+//  callback();
+//}
 })();
