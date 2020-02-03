b85ecc22d37a583a259dc4e35a865f9110d563c1
rodriguestiago <tiagogameiro15@gmail.com>
2020-02-03 14:39:15 +0100

    jasmine 2 finished

--- 2.exercisesNext/3.0.Test1/questions.js
+++ 2.exercisesNext/3.0.Test1/questions.js
@@ -12,55 +12,77 @@ let remplaceECar = (texte) => {
     return text;
 }
 let concatString = (texte1, texte2) => {
-
+    return texte1 + texte2;
 }
 let afficherCar5 =  (texte) => {
-
+    return texte.slice(4,5);
 }
 let afficher9Car =  (texte) => {
-
+    return texte.slice(0,9);
 }
 let majusculeString =  (texte) => {
-
+    return texte.toUpperCase();
 }
 let minusculeString =  (texte) => {
-
+    return texte.toLowerCase();
 }
 let SupprEspaceString =  (texte) => {
-
+    return texte.trim();
 }
 let IsString =  (texte) => {
-
+//    console.log(texte);
+//    texte.includes("Est ce une chaine de caractères ?");
+    
+    if (typeof texte == "string"){
+        return true;
+    }
 }
 
 let AfficherExtensionString =  (texte) => {
-
+    return texte.split('.').pop();
 }
 let NombreEspaceString =  (texte) => {
-
+    return texte.split(" ").length - 1;
 }
 let InverseString =  (texte) => {
-
+    return texte.split('').reverse().join('');
 }
 
 /**
  * Exercices sur les nombres et les caluls mathématiques
  */
 let calculPuissance =  (x, y) => {
-
+//    let value = Math.pow(2, 3);
+//    return value;
+    return Math.pow(2, 3);
 }
 let valeurAbsolue =  (nombre) => {
-
+    return Math.abs(nombre);
 }
 let valeurAbsolueArray =  (array) => {
+    
 
+    
 }
 let sufaceCercle =  (rayon) => {
-
+    //5
+//    let area = Math.PI * rayon * rayon;
+//    console.log(area);
+//    
+//     let areaRounded = Math.ceil(area * 100) / 100;
+//    console.log(areaRounded);
+    
+    console.log(Math.PI * rayon * rayon);
+    console.log(Math.round(Math.PI * rayon * rayon * 100) / 100);
+    
 }
 let hypothenuse =  (ab, ac) => {
-
+    //5 et 8
+    return Math.sqrt(ab*ab + ac*ac);
 }
 let calculIMC =  (poids, taille) => {
-
+    let finalPoids = poids * .45;
+    let finalTaille = taille * .025;
+    console.log(finalPoids / Math.pow(finalTaille, 2));
+    return finalPoids / Math.pow(finalTaille, 2);
 }
--- 2.exercisesNext/3.1.Test2/questions.js
+++ 2.exercisesNext/3.1.Test2/questions.js
@@ -1,43 +1,75 @@
 let CreationTableauLangages =  () => {
-
+    let langages = ["Html", "CSS", "Java", "PHP"];
+    return langages;
 }
 
 let CreationTableauNombres =  () => {
-
+    let nombres = [0, 1, 2, 3, 4, 5];
+    return nombres;
 }
 
 let RemplacementElement =  (langages) => {
-
+    langages[2] = "Javascript";
+    return langages;
+    
+  //  return langages.replace(/Java/i, 'Javascript');
+    
+    //console.log(langages[1]);
 }
 
 let AjoutElementLangages =  (langages) => {
-
+    langages.push("Ruby");
+    langages.push("Python");
+    return langages;
 }
 
 let AjoutElementNombres =  (nombres) => {
-
+    nombres.unshift(-2);
+    nombres.unshift(-1);
+    nombres.sort((a, b) => a - b);
+    return nombres;
+   
 }
 
 let SuppressionPremierElement =  (langages) => {
-
+    langages.shift();
+    return langages;
 }
 
 let SuppressionDernierElement =  (langages) => {
-
+    langages.pop();
+    return langages;
 }
 
 let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
-
+    let reseaux_sociaux = reseaux_sociaux_chaine.split(',');
+    return reseaux_sociaux;
+    
+    
+//    console.log(reseaux_sociaux);
+    
+    
+  //  let reseaux_sociaux = reseaux_sociaux_chaine.split(" ");
+    //return reseaux_sociaux;
+    
+    //console.log(reseaux_sociaux);
+    
+    //let reseaux_sociaux = reseaux_sociaux_chaine.split(', '); 
+   // return reseaux_sociaux;
+   // console.log(reseaux_sociaux);
 }
 
 let ConversionTableauChaine =  (langages) => {
-
+    let langages_chaine = langages.toString();
+    return langages_chaine;
 }
 
 let TriTableau =  (reseaux_sociaux) => {
-
+    reseaux_sociaux.sort();
+    return reseaux_sociaux;
 }
 
 let InversionTableau =  (reseaux_sociaux) =>{
-
+    reseaux_sociaux.reverse();
+    return reseaux_sociaux;
 }
